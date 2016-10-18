7ab45e2eb269c809e9cc79dd1c0282fea202e55a
Carlos Nascimento <cnbatalha@gmail.com>
2016-10-18 00:03:53 -0400

    led ligt up when press button

---
+++ projects-learn/press-btn-led/press_button_led/press_button_led.ino
@@ -0,0 +1,25 @@
+/* Arduino */
+
+const int ledPin = 13; // led no pino 13
+const int botao = 2; // botao no pino 2
+int estadoBotao = 0; // status do botao press/not press
+
+void setup(){
+  pinMode(ledPin, OUTPUT); // pino com led sera saida
+  pinMode(botao, INPUT);// pino com botao sera a entrada
+}
+
+void loop()
+{
+  // capturando estado do botao
+  estadoBotao = digitalRead(botao);
+  
+  if (estadoBotao == HIGH)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+}
