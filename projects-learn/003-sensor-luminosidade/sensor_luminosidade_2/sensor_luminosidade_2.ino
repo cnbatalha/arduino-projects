48c4e2ed3bfbe45951e9ada5e916a431d7171e42
Carlos Nascimento <cnbatalha@gmail.com>
2016-10-19 22:29:23 -0400

    sensor luminosidade com led

---
+++ projects-learn/003-sensor-luminosidade/sensor_luminosidade_2/sensor_luminosidade_2.ino
@@ -0,0 +1,28 @@
+// sensor luminosidade
+
+const int LDR = 0;
+const int LED = 12;
+int valorLido = 0;
+
+void setup()
+{
+  Serial.begin(9600);  
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  valorLido = analogRead(LDR);
+  Serial.print("valor lido");
+  Serial.println(valorLido);
+  delay(500);
+  
+  if ( valorLido < 400)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+}
