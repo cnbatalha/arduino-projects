cdbd1685f85d45225a72fcd7721eb3cc08ae9a67
Carlos Nascimento <cnbatalha@gmail.com>
2016-10-26 19:49:57 -0400

    add exemplo com display  segmentos

---
+++ projects-learn/005-display-7-segmentos/numeros_simples/numeros_simples.ino
@@ -0,0 +1,66 @@
+/* DISPLAY  SEGMENTOS */
+
+const int A = 12;
+const int B = 11;
+const int C = 10;
+const int D = 9;
+const int E = 8;
+const int F = 6;
+const int G = 7;
+
+
+void setup()
+{
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+}
+
+void loop()
+{
+  // zero
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  delay(1000);
+
+  // 1 
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  delay(1000);
+
+  // 2
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  delay(1000);
+  
+  // 3
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  delay(1000);  
+  
+}
+
